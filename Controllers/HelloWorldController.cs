8a85579bdccb054069afe71d883d2eb9f8f0db9e
Alexandre Ferreira <alexandre@pelago.com.br>
2020-02-23 01:22:03 -0300

    4 - View básico

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,9 +6,9 @@ namespace mvc_movie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWord/
-        public string Index()
+        public IActionResult Index()
         {
-            return "Está é a minha ação padrão.";
+            return View();
         }
 
         // GET: /HelloWorld/Welcome
