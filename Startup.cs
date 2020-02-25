0122efbe6d9e430f7e52f029f5322ea7a99bcd0a
Alexandre Ferreira <alexandre@pelago.com.br>
2020-02-24 23:26:12 -0300

    10 - Injeção de dependência em Startup

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using mvc_movie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace mvc_movie
 {
@@ -24,6 +26,8 @@ namespace mvc_movie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<FilmeContext>(options => options.UseSqlite(Configuration.GetConnectionString("FilmeContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
